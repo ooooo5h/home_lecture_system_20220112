282486e45df94b7056097ccb5e5c46e83eab1602
전은형 <ooooo5h91@gmail.com>
2022-01-12 22:57:08 +0900

    for list comprehension 문법 적용
    - 동작 속도 향상

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -12,15 +12,10 @@ def test():
     db.cursor.execute(sql)
     all_list = db.cursor.fetchall()
     
-    all_users = []
-    
-    for row in all_list :
-        # Users(row) : Users형태의 인스턴스 생성 => 함수들도 내장하고 있다
-        # 인스턴스에게 곧바로 get_data_object() 실행 명령
-        # 해당 유저의 정보를 활용한 dict가 리턴됨
-        # 곧바로 목록의 append의 재료로 활용
-        all_users.append(Users(row).get_data_object())
-    
+    # 목록 for문을 돌면서, 한 줄을 row로 추출하고, 추출된 row를 모델클래스로 가공해서 dict로 재가공해라를 한줄로 작성
+    # comprehension
+    all_users = [ Users(row).get_data_object()  for row in all_list]
+        
     return{
         'users' : all_users
     }
