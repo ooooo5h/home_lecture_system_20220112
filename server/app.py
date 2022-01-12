71af343bc604faa12ac0b52bb8bb3f4842c2a94b
전은형 <ooooo5h91@gmail.com>
2022-01-12 22:26:27 +0900

    app.py -> db변수 추가 -> 가져다 쓸 수 있게 import
    - DBConnector클래스로, ,연결정보를 일원화

--- server/app.py
+++ server/app.py
@@ -1,6 +1,11 @@
 # 플라스크 자체를 로딩하자
 from flask import Flask
 
+from server.db_connector import DBConnector
+
+# DB연결 정보를 관리하는 클래스 생성해서 그 객체를 변수에 담자
+db = DBConnector()
+
 def created_app():
     app = Flask(__name__)
     
