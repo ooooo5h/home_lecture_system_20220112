902442f4f0cbce10af52a38986a747b07944c092
전은형 <ooooo5h91@gmail.com>
2022-01-12 22:41:10 +0900

    DB호출
    - API 파일에서 별도로 db변수 생성

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,6 +1,18 @@
 # 로그인/회원가입 등, 사용자 정보 관련 기능 모아두는 모듈
+# DB 연결 정보를 보관하는 변수를 import 하자
+from server.db_connector import DBConnector
+
+db = DBConnector()
 
 def test():
+    
+    # DB의 모든 users 조회 쿼리를 날려보자
+    sql = "SELECT * FROM users"
+    db.cursor.execute(sql)
+    all_list = db.cursor.fetchall()
+    
+    print(all_list)
+    
     return{
         '임시' : '임시'
     }
--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from server.db_connector import DBConnector
 from .api.user import test
 
 # DB연결 정보를 관리하는 클래스 생성해서 그 객체를 변수에 담자
-db = DBConnector()
+# db = DBConnector()
 
 def created_app():
     app = Flask(__name__)
