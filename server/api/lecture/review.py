c4d14e04181d1d626d913d4694aef5f8d6c6ff36
전은형 <ooooo5h91@gmail.com>
2022-01-13 21:54:28 +0900

    파라미터/db내용 검증 로직
    - 모든 검증을 통과해야만 리뷰 작성 가능

--- server/api/lecture/review.py
+++ server/api/lecture/review.py
@@ -2,15 +2,43 @@ from server import db
 
 # 강의의 리뷰에 대한 기능만 모아두는 파일
 
-def write_review(params):
-    
+def write_review(params):  
 
     # 1. 평점은 1 ~ 5 사이만 가능(파라미터)
-    # 2. 제목의 길이는 최소 5자 이상(파라미터)
-    # 3. 내용의 길이는 최소 10자 이상(파라미터)
+    score = float(params['score'])   # 파라미터들은 기본적으로 str형으로 들어오기 때문에 float로 형변환   
+    if not (1 <= score <= 5):
+        return{
+            'code' : 400,
+            'message' : '평점은 1 ~ 5 사이여야 함'
+        }, 400
+    
+    # 2. 제목의 길이는 최소 5자 이상(파라미터)    
+    if len(params['title']) < 5:
+        return{
+            'code' : 400,
+            'meesage' : '제목 5자 이상 작성해라',
+        }, 400
+    
+    # 3. 내용의 길이는 최소 10자 이상(파라미터)   
+    if len(params['content']) < 10:
+        return{
+        'code' : 400,
+        'meesage' : '내용 10자 이상 작성해라',
+    }, 400
+        
     # 4. 수강신청을 해야지 리뷰 작성 가능(DB내부 조회)
+    sql = f"SELECT * FROM lecture_user WHERE lecture_id = {params['lecture_id']} AND user_id = {params['user_id']}"
+    
+    query_result = db.executeOne(sql)
+    
+    if not query_result :
+        return {
+            'code' : 400,
+            'message' : '수강신청을 안했으니 리뷰 작성 못해요.'
+        }, 400
     
     # 리뷰 실제 등록
     return{
-        '임시' : '강의 리뷰 작성'
+        'code' : 200,
+        'message' : '리뷰 작성 완료'
     }
