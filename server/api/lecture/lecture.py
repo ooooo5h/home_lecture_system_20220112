a9a523447760dbe771b86f860b821b8e381bbc27
전은형 <ooooo5h91@gmail.com>
2022-01-13 21:15:12 +0900

    모든 강의 목록 조회 기능 추가
    - 단순 접속만 열어두었음

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -1,6 +1,9 @@
 from server.model import Lectures
 from server import db
 
-def get_all_lectures():
-    pass
+# 모든 강의 목록 내려오기(당장은 params 활용 x)
+def get_all_lectures(params):
+    return{
+        '임시' : '강의 목록 가져오기'
+    }
 
