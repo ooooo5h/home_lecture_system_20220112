be24c3f76d3a0f4e4cceb82127038238ab9162ec
전은형 <ooooo5h91@gmail.com>
2022-01-13 22:01:16 +0900

    추가 검증 로직 작성
    - 이미 리뷰를 남겼다면 못쓰게 막음

--- server/api/lecture/review.py
+++ server/api/lecture/review.py
@@ -37,7 +37,22 @@ def write_review(params):
             'message' : '수강신청을 안했으니 리뷰 작성 못해요.'
         }, 400
     
+    # 5. 이미 리뷰를 등록했다면, 추가 등록 못하게 막자
+    sql = f"SELECT * FROM lecture_review WHERE lecture_id = {params['lecture_id']} AND user_id = {params['user_id']}"
+    
+    already_review_data = db.executeOne(sql)
+    
+    if already_review_data:
+        return{
+            'code' : 400,
+            'message' : '이미 작성했습니다.'
+        }, 400
+    
     # 리뷰 실제 등록
+    sql = f"INSERT INTO lecture_review (lecture_id, user_id, title, content, score) VALUES ({params['lecture_id']}, {params['user_id']}, '{params['title']}', '{params['content']}',{params['score']})"
+    
+    db.insertAndCommit(sql)
+    
     return{
         'code' : 200,
         'message' : '리뷰 작성 완료'
